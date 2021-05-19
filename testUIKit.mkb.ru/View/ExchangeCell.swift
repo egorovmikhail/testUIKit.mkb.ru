b6c985f6a848449f19377286d7fd717e16ad10b2
Михаил <egorovmikhail@bk.ru>
2021-05-20 02:41:38 +1100

    редактировал ячейку, изменил адаптив контента для альбомной ориентации и расстановку компонентов, размер и цвет шрифта

--- testUIKit.mkb.ru/View/ExchangeCell.swift
+++ testUIKit.mkb.ru/View/ExchangeCell.swift
@@ -17,11 +17,10 @@ class ExchangeCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubview(currName)
-        setTitleLabelConstraints()
         setFromTo()
-        setsale()
+        setCurrName()
         setbuy()
+        setsale()
     }
     
     required init?(coder: NSCoder) {
@@ -35,35 +34,39 @@ class ExchangeCell: UITableViewCell {
         buy.text = rate.buyText
     }
     
-    func setTitleLabelConstraints() {
-        addSubview(currName)
-        currName.numberOfLines = 2
-        currName.translatesAutoresizingMaskIntoConstraints = false
-        currName.topAnchor.constraint(equalTo: topAnchor, constant: 6).isActive = true
-        currName.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 17).isActive = true
-        currName.widthAnchor.constraint(equalToConstant: contentView.bounds.width - 100).isActive = true
-    }
-    
     func setFromTo() {
         addSubview(fromTo)
+        fromTo.font = .boldSystemFont(ofSize: 24)
         fromTo.translatesAutoresizingMaskIntoConstraints = false
-        fromTo.topAnchor.constraint(equalTo: topAnchor,constant: 10).isActive = true
-        fromTo.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        fromTo.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        fromTo.topAnchor.constraint(equalTo: topAnchor,constant: 15).isActive = true
+        fromTo.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        fromTo.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 17).isActive = true
+    }
+    
+    func setCurrName() {
+        addSubview(currName)
+        currName.numberOfLines = 2
+        currName.textAlignment = .left
+        currName.translatesAutoresizingMaskIntoConstraints = false
+        currName.topAnchor.constraint(equalTo: topAnchor, constant: 6).isActive = true
+        currName.leadingAnchor.constraint(equalTo: fromTo.trailingAnchor).isActive = true
+        currName.widthAnchor.constraint(equalToConstant: contentView.bounds.width - 100).isActive = true
     }
     
     func setsale() {
         addSubview(sale)
-        sale.textColor = .red
+        sale.textColor = .systemRed
+        sale.font = UIFont(name: "arial", size: 14)
         sale.translatesAutoresizingMaskIntoConstraints = false
         sale.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
-        sale.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 17).isActive = true
+        sale.trailingAnchor.constraint(equalTo: buy.leadingAnchor).isActive = true
         sale.widthAnchor.constraint(equalToConstant: bounds.width / 2).isActive = true
     }
     
     func setbuy() {
         addSubview(buy)
-        buy.textColor = .blue
+        buy.textColor = .systemBlue
+        buy.font = UIFont(name: "arial", size: 14)
         buy.translatesAutoresizingMaskIntoConstraints = false
         buy.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
         buy.widthAnchor.constraint(equalToConstant: bounds.width / 2).isActive = true

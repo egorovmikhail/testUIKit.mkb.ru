4c856a5408280d0a8a015efa14f149f72de74a31
Михаил <egorovmikhail@bk.ru>
2021-05-21 03:35:16 +1100

    В сессии изменил значения header на словарь

--- testUIKit.mkb.ru/NetworkService.swift
+++ testUIKit.mkb.ru/NetworkService.swift
@@ -14,15 +14,11 @@ class NetworkService {
     guard let url = URL(string: "https://alpha.as50464.net:29870/moby-pre-44/core?r=BEYkZbmV&d=563B4852-6D4B-49D6-A86E-B273DD520FD2&t=ExchangeRates&v=44") else { return }
     
     let jsonBody = ["uid":"563B4852-6D4B-49D6-A86E-B273DD520FD2","type":"ExchangeRates","rid":"BEYkZbmV"]
+    let heder = ["User-Agent":"Test GeekBrains iOS 3.0.0.182 (iPhone 11; iOS 14.4.1; Scale/2.00; Private)", "Content-Type":"application/json", "Accept":"application"]
     
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-    request.addValue("Test GeekBrains iOS 3.0.0.182 (iPhone 11; iOS 14.4.1; Scale/2.00; Private)",
-                     forHTTPHeaderField: "User-Agent")
-    request.addValue("application/json",
-                     forHTTPHeaderField: "Content-Type")
-    request.addValue("Accept",
-                     forHTTPHeaderField: "application")
+    request.allHTTPHeaderFields = heder
     
     guard let httpBody = try? JSONSerialization.data(withJSONObject: jsonBody, options: []) else { return }
     request.httpBody = httpBody
@@ -30,7 +26,7 @@ class NetworkService {
     let session = URLSession.shared
     session.dataTask(with: request) { (data, response, error) in
       DispatchQueue.global().async {
-        guard let data = data else { return }
+        guard let data = data else { return print("Что-то пошло не так", error as Any)}
         
         do {
           let exchangeRates = try JSONDecoder().decode(ExchangeRates.self, from: data)
@@ -39,7 +35,7 @@ class NetworkService {
           }
         } catch {
             completion(nil, true)
-            print(error)  
+            print(error)
         }
       }
     } .resume()
